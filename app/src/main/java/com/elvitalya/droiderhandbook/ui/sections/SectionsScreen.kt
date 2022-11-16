506934f9e46e3e2a3f952a6d4a2500d717d00a7e
vitaly <kuraev_vf@mail.ru>
2022-11-16 23:46:57 +0500

    in progress

--- app/src/main/java/com/elvitalya/droiderhandbook/ui/sections/SectionsScreen.kt
+++ app/src/main/java/com/elvitalya/droiderhandbook/ui/sections/SectionsScreen.kt
@@ -1,12 +1,10 @@
 package com.elvitalya.droiderhandbook.ui.sections
 
-import android.util.Log
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -24,7 +22,6 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.elvitalya.droiderhandbook.data.model.QuestionEntity
-import com.elvitalya.droiderhandbook.ui.main.MainActivity.Companion.TAG
 
 @Composable
 fun SectionsScreen(
@@ -40,13 +37,13 @@ fun SectionsScreen(
     val kotlinQuestions by vm.kotlinQuestions.collectAsState()
     val basicQuestions by vm.basicQuestions.collectAsState()
 
-    var selectedSection by remember { mutableStateOf<String?>(null) }
+    val loading by vm.loading.collectAsState()
+
+    var selectedSection by remember { mutableStateOf<Sections?>(null) }
 
 
     LaunchedEffect(key1 = Unit) {
-        Log.d(TAG, "SectionsScreen: Launch effect called!")
         vm.getQuestions()
-        vm.getSections()
     }
 
     Box(
@@ -54,107 +51,161 @@ fun SectionsScreen(
             .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Crossfade(targetState = javaQuestions.isNotEmpty()) { notEmpty ->
-            if (notEmpty) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
-                    Sections.values().forEach { section ->
-                        SectionItem(
-                            title = section.name,
-                            questions = when (section) {
-                                Sections.Java -> javaQuestions
-                                Sections.Kotlin -> kotlinQuestions
-                                Sections.Android -> androidQuestions
-                                Sections.Basic -> basicQuestions
-                            },
-                            onQuestionClick = onQuestionClick,
-                            onSectionSelected = {
-                                selectedSection = if (it == selectedSection) null else it
-                            },
-                            selectedSection = selectedSection
-                        )
-                    }
+        if (loading.not()) {
+            Content(
+                javaQuestions = javaQuestions,
+                kotlinQuestions = kotlinQuestions,
+                androidQuestions = androidQuestions,
+                basicQuestions = basicQuestions,
+                onSectionSelected = {
+                    selectedSection =
+                        if (it == selectedSection) null else it
+                },
+                onQuestionClick = onQuestionClick,
+                selectedSection = selectedSection
+            )
+        } else {
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.tertiary,
+                modifier = Modifier
+                    .size(150.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun Content(
+    javaQuestions: List<QuestionEntity>,
+    kotlinQuestions: List<QuestionEntity>,
+    androidQuestions: List<QuestionEntity>,
+    basicQuestions: List<QuestionEntity>,
+    onQuestionClick: () -> Unit,
+    onSectionSelected: (Sections) -> Unit,
+    selectedSection: Sections?
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        when (selectedSection) {
+            null -> {
+                Sections.values().forEach { section ->
+                    SectionItem(
+                        section = section,
+                        questions = when (section) {
+                            Sections.Java -> javaQuestions
+                            Sections.Kotlin -> kotlinQuestions
+                            Sections.Android -> androidQuestions
+                            Sections.Basic -> basicQuestions
+                        },
+                        onQuestionClick = onQuestionClick,
+                        onSectionSelected = onSectionSelected,
+                        selectedSection = selectedSection
+                    )
                 }
-            } else {
-                CircularProgressIndicator(
-                    color = MaterialTheme.colorScheme.tertiary,
-                    modifier = Modifier
-                        .size(150.dp)
+            }
+            Sections.Java -> {
+                SectionItem(
+                    section = Sections.Java,
+                    questions = javaQuestions,
+                    onQuestionClick = onQuestionClick,
+                    onSectionSelected = onSectionSelected,
+                    selectedSection = selectedSection
+                )
+            }
+            Sections.Kotlin -> {
+                SectionItem(
+                    section = Sections.Kotlin,
+                    questions = kotlinQuestions,
+                    onQuestionClick = onQuestionClick,
+                    onSectionSelected = onSectionSelected,
+                    selectedSection = selectedSection
+                )
+            }
+            Sections.Android -> {
+                SectionItem(
+                    section = Sections.Android,
+                    questions = androidQuestions,
+                    onQuestionClick = onQuestionClick,
+                    onSectionSelected = onSectionSelected,
+                    selectedSection = selectedSection
+                )
+            }
+            Sections.Basic -> {
+                SectionItem(
+                    section = Sections.Basic,
+                    questions = basicQuestions,
+                    onQuestionClick = onQuestionClick,
+                    onSectionSelected = onSectionSelected,
+                    selectedSection = selectedSection
                 )
             }
         }
     }
-
 }
 
 @Composable
 private fun SectionItem(
-    title: String,
+    section: Sections,
     questions: List<QuestionEntity>,
     onQuestionClick: () -> Unit,
-    onSectionSelected: (String) -> Unit,
-    selectedSection: String?
+    onSectionSelected: (Sections) -> Unit,
+    selectedSection: Sections?
 ) {
-    Log.d(TAG, "SectionItem: $selectedSection")
-    var isExpanded by remember { mutableStateOf(false) }
-    val visible = selectedSection == title || selectedSection == null
-    if (visible) {
-        Text(
-            text = title,
+    Text(
+        text = section.name,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .background(
+                MaterialTheme.colorScheme.tertiary,
+                RoundedCornerShape(16.dp)
+            )
+            .clip(RoundedCornerShape(16.dp))
+            .clickable(indication = null, interactionSource = MutableInteractionSource()) {
+                onSectionSelected(section)
+            }
+            .padding(16.dp),
+        fontSize = 28.sp,
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colorScheme.onTertiary
+    )
+    if (selectedSection == section) {
+        LazyColumn(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp)
-                .background(
-                    MaterialTheme.colorScheme.tertiary,
-                    RoundedCornerShape(16.dp)
-                )
-                .clip(RoundedCornerShape(16.dp))
-                .clickable {
-                    isExpanded = isExpanded.not()
-                    onSectionSelected(title)
-                }
-                .padding(16.dp),
-            fontSize = 28.sp,
-            textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.onTertiary
-        )
-        AnimatedVisibility(isExpanded) {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
-            ) {
-                items(questions) { question ->
+                .padding(horizontal = 16.dp),
+        ) {
+            items(questions) { question ->
 
-                    val text =
-                        question.text.run { if (length > 50) this else this.substring(0, 50) }
+                val text =
+                    question.text.run { if (length > 50) this else this.substring(0, 50) }
 
-                    Column(
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { onQuestionClick() }
+                        .padding(4.dp)
+                        .border(
+                            BorderStroke(1.dp, MaterialTheme.colorScheme.tertiary),
+                            RoundedCornerShape(16.dp)
+                        )
+                ) {
+                    Text(
+                        text = question.title ?: "", modifier = Modifier
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = text,
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable { onQuestionClick() }
-                            .padding(4.dp)
-                            .border(
-                                BorderStroke(1.dp, MaterialTheme.colorScheme.tertiary),
-                                RoundedCornerShape(16.dp)
-                            )
-                    ) {
-                        Text(
-                            text = question.title ?: "", modifier = Modifier
-                                .padding(8.dp)
-                        )
-                        Text(
-                            text = text,
-                            modifier = Modifier
-                                .padding(8.dp)
-                        )
+                            .padding(8.dp)
+                    )
 
-                    }
                 }
             }
         }
+
     }
 
 }
--- app/src/main/java/com/elvitalya/droiderhandbook/ui/sections/SectionsViewModel.kt
+++ app/src/main/java/com/elvitalya/droiderhandbook/ui/sections/SectionsViewModel.kt
@@ -8,6 +8,7 @@ import com.elvitalya.droiderhandbook.data.model.QuestionEntity
 import com.elvitalya.droiderhandbook.ui.main.MainActivity.Companion.TAG
 import com.elvitalya.droiderhandbook.utils.FireBaseHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.count
@@ -26,35 +27,28 @@ class SectionsViewModel @Inject constructor(
     val basicQuestions = MutableStateFlow<List<QuestionEntity>>(emptyList())
     val androidQuestions = MutableStateFlow<List<QuestionEntity>>(emptyList())
 
-    fun getSections() {
-        FireBaseHelper.mainSection
-            .get()
-            .addOnSuccessListener { snapShot ->
-                sectionsList.value = snapShot.documents.map { documentSnapshot ->
-                    documentSnapshot.data?.values?.first().toString()
-                }
-
-            }
-    }
+    val loading = MutableStateFlow(true)
 
     fun getQuestions() {
         viewModelScope.launch {
             try {
-                dataRepository.getQuestions().collect { list ->
-                    if (list.isEmpty()) dataRepository.loadQuestions()
+                loading.value = true
+                dataRepository.getQuestions().collect { questions ->
+                    if (questions.isEmpty()) dataRepository.loadQuestions()
                     else {
-                        Log.d(TAG, "getQuestions: list - $list")
                         javaQuestions.value =
-                            list.filter { question -> question.id in FireBaseHelper.javaQuestionsIds }
+                            questions.filter { question -> question.id in FireBaseHelper.javaQuestionsIds }
                         kotlinQuestions.value =
-                            list.filter { question -> question.id in FireBaseHelper.kotlinQuestionsIds }
+                            questions.filter { question -> question.id in FireBaseHelper.kotlinQuestionsIds }
                         basicQuestions.value =
-                            list.filter { question -> question.id in FireBaseHelper.basicQuestionsIds }
+                            questions.filter { question -> question.id in FireBaseHelper.basicQuestionsIds }
                         androidQuestions.value =
-                            list.filter { question -> question.id in FireBaseHelper.androidQuestionsIds }
+                            questions.filter { question -> question.id in FireBaseHelper.androidQuestionsIds }
+                        loading.value = false
                     }
                 }
             } catch (e: Exception) {
+                loading.value = false
                 Log.d(TAG, "getQuestions: error ${e.message}")
             }
 
