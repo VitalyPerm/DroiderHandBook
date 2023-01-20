810fb7aa06410cd0131b340e7a40b16753cfebb7
vitaly <kuraev_vf@mail.ru>
2023-01-20 16:14:50 +0500

    in progress

--- app/src/main/java/com/elvitalya/droiderhandbook/utils/FireBaseHelper.kt
+++ app/src/main/java/com/elvitalya/droiderhandbook/utils/FireBaseHelper.kt
@@ -1,10 +1,12 @@
 package com.elvitalya.droiderhandbook.utils
 
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 object FireBaseHelper {
     val questions = Firebase.firestore.collection("questions")
+        .orderBy("id", Query.Direction.ASCENDING)
 
     const val javaQuestionsPrefix = "a"
     const val kotlinQuestionsPrefix = "b"
